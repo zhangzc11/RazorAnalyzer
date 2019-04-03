c23373096164af340f59d09d28a829154b56cc85
Zhicai <zzhang2@caltech.edu>
2019-04-03 15:55:25 -0700

    cleanup 2016 v4p1 list

--- lists/Run2/razorNtuplerV4p1/Data_2016_reMINIAOD/dump_2016official_DoubleEG_T2.sh
+++ lists/Run2/razorNtuplerV4p1/Data_2016_reMINIAOD/dump_2016official_DoubleEG_T2.sh
@@ -4,8 +4,8 @@ baseDir=/mnt/hadoop/store/group/phys_susy/razor/run2/Run2RazorNtupleV4.1/Data/Do
 
 for run in 2016B_ver1 2016B_ver2 2016C 2016D 2016E 2016F 2016G 2016H
 do
-	echo 'xxxxxxxxxx' > DoubleEG_${run}_06Aug2018.cern.txt
-	subBase1=${baseDir}crab_prod_CaltechT2_Run2RazorNtuplerV4p1_DoubleEG_${run}_ReMiniAOD2016Official_v1_06Aug2018/
+	echo 'xxxxxxxxxx' > DoubleEG_${run}_25Mar2019.caltech.txt
+	subBase1=${baseDir}crab_prod_CaltechT2_Run2RazorNtuplerV4p1_DoubleEG_${run}_ReMiniAOD2016Official_v1_25Mar2019/
 	echo ${subBase1}
 	for sub1 in `ls ${subBase1}`
 	do
@@ -17,9 +17,9 @@ do
 			echo ${subBase3}
 			for ifile in `ls ${subBase3} | grep root`
 			do
-				echo "${subBase3}${ifile}" >> DoubleEG_${run}_06Aug2018.cern.txt
+				echo "${subBase3}${ifile}" >> DoubleEG_${run}_25Mar2019.caltech.txt
 			done
 		done
 	done
-	sed -i '/xxxxxxxxxx/d' DoubleEG_${run}_06Aug2018.cern.txt
+	sed -i '/xxxxxxxxxx/d' DoubleEG_${run}_25Mar2019.caltech.txt
 done
--- lists/Run2/razorNtuplerV4p1/Data_2016_reMINIAOD/dump_2016official_SingleElectron_T2.sh
+++ lists/Run2/razorNtuplerV4p1/Data_2016_reMINIAOD/dump_2016official_SingleElectron_T2.sh
@@ -4,8 +4,8 @@ baseDir=/mnt/hadoop/store/group/phys_susy/razor/run2/Run2RazorNtupleV4.1/Data/Si
 
 for run in 2016B_ver1 2016B_ver2 2016C 2016D 2016E 2016F 2016G 2016H
 do
-	echo 'xxxxxxxxxx' > SingleElectron_${run}_06Aug2018.cern.txt
-	subBase1=${baseDir}crab_prod_CaltechT2_Run2RazorNtuplerV4p1_SingleElectron_${run}_ReMiniAOD2016Official_v1_06Aug2018/
+	echo 'xxxxxxxxxx' > SingleElectron_${run}_18Mar2019.caltech.txt
+	subBase1=${baseDir}crab_prod_CaltechT2_Run2RazorNtuplerV4p1_SingleElectron_${run}_ReMiniAOD2016Official_v1_18Mar2019/
 	echo ${subBase1}
 	for sub1 in `ls ${subBase1}`
 	do
@@ -17,9 +17,9 @@ do
 			echo ${subBase3}
 			for ifile in `ls ${subBase3} | grep root`
 			do
-				echo "${subBase3}${ifile}" >> SingleElectron_${run}_06Aug2018.cern.txt
+				echo "${subBase3}${ifile}" >> SingleElectron_${run}_18Mar2019.caltech.txt
 			done
 		done
 	done
-	sed -i '/xxxxxxxxxx/d' SingleElectron_${run}_06Aug2018.cern.txt
+	sed -i '/xxxxxxxxxx/d' SingleElectron_${run}_18Mar2019.caltech.txt
 done
